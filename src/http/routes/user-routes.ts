384dbf55e795f4985dca5d97b86e2d27d42fcb02
Paulo Turco <pauloturcko@gmail.com>
2026-01-13 19:52:08 -0300

    feat: #0004 → Cria rota de criação de usuário

--- src/http/routes/user-routes.ts
+++ src/http/routes/user-routes.ts
@@ -4,6 +4,6 @@ import { UserController } from "../controllers/user-controller.js";
 const userRouter = Router();
 const userController = new UserController();
 
-userRouter.post("/register", userController.register);
+userRouter.post("/register", (req, res) => userController.register(req, res));
 
 export { userRouter };
