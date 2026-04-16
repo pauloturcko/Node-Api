6605ce74fa5e513da61539c81f2eef699399c75d
Paulo Turco <pauloturcko@gmail.com>
2026-04-15 22:15:25 -0300

    feat: #0015 - Implementa CRUD de likes pt.02

--- src/http/controllers/follows-controller.ts
+++ src/http/controllers/follows-controller.ts
@@ -43,4 +43,56 @@ export class FollowsController {
       message: "Follow was added",
     });
   }
+
+  async deleteFollow(req: Request, res: Response) {
+    const followerId = req.user?.id;
+    if (!followerId) {
+      res.status(401).json({
+        message: "Unauthorized",
+      });
+      return;
+    }
+
+    const followingId = req.params.followingId;
+    if (!followingId) {
+      res.status(400).json({
+        message: "Following Id is required",
+      });
+      return;
+    }
+
+    const alreadyFollow = await this.followsRepository.loadFollow(
+      followerId,
+      Number(followingId),
+    );
+
+    if (!alreadyFollow) {
+      res.status(404).json({
+        message: "You can unfollow only users that you follow",
+      });
+      return;
+    }
+
+    await this.followsRepository.deleteFollow(followerId, Number(followingId));
+
+    res.status(200).json({
+      message: "User was unfollowed",
+    });
+  }
+
+  async getFollowers(req: Request, res: Response) {
+    const userId = Number(req.params.id);
+
+    const followers = await this.followsRepository.loadFollowers(userId);
+
+    res.status(200).json(followers);
+  }
+
+  async getFollowing(req: Request, res: Response) {
+    const userId = Number(req.params.id);
+
+    const following = await this.followsRepository.loadFollowing(userId);
+
+    res.status(200).json(following);
+  }
 }
